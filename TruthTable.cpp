b7b31ab2114bd243a3101b5d476fb3227b479ee2
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-21 11:19:43 +0530

    Create TruthTable.cpp

---
+++ TruthTable.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+    int x,y,z;
+    cout<<"  X  |  Y  |  Z  |  XY+Z  ";
+    for(x=0;x<=1;++x)
+        for(y=0;y<=1;++y)
+            for(z=0;z<=1;++z)
+            {
+                if(x*y+z==2)
+                    cout<<endl<<"  "<< x <<"  |  "<< y <<"  |  "<< z <<"  |   1";
+                else
+                    cout<<endl<<"  "<< x <<"  |  "<< y <<"  |  "<< z <<"  |   "<<x*y+z;
+            }
+}
