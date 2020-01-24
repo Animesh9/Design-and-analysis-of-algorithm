7deadeb85780180a319ad7b83375b53feff2b3ae
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-24 11:26:33 +0530

    Update Structure.c

--- Structure.c
+++ Structure.c
@@ -6,3 +6,12 @@ struct DevOps
     char n[15];
     int sc;
 };
+
+int main()
+{
+    struct DevOps s1;
+    struct DevOps *p;
+    p = &s1;
+    scanf ("%d%s%d",&p->si,&p->n,&p->sc);
+    printf("\n%d\n%s\n%d\n",p->si,p->n,p->sc);
+}
