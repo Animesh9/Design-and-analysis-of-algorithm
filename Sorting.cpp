849d905aee572ca087b500460f65cb8d73240e35
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-25 13:12:08 +0530

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -37,4 +37,12 @@ void printArray(int arr[], int size)
         cout << arr[i] << " ";
     cout << endl;
 }
-
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
