fe01d571adfccb0da1a1e644883aedc292f83b3f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-27 13:09:58 +0530

    Update OnesComplement.c

--- OnesComplement.c
+++ OnesComplement.c
@@ -6,3 +6,21 @@ int main()
     int i, e=0;
     printf("Enter %d binary values: ",SIZE);
     gets(b);
+    for(i=0;i<SIZE;i++)
+    {
+        if(b[i]=='1')
+        {
+            oc[i]='0';
+        }
+        else if(b[i]=='0')
+        {
+            oc[i]='1';
+        }
+        else
+        {
+            printf("2-9 and alphabets are not binary numbers");
+            e=1;
+            break;
+        }
+    }
+}
