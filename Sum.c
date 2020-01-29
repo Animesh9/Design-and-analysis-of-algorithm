82ed563bf765f10b9cf89e2ecf8e409e17a9a848
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-29 11:41:38 +0530

    Create Sum.c

---
+++ Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main ()
+{
+    int A[3][3],B[3][3],C[3][3],i,j;
+    printf("Enter 9 numbers for first matrix");
+    for(i=0;i<=2;i++)
+        for(j=0;j<=2;j++)
+        scanf("%d",&A[i][j]);
+    printf("Enter 9 numbers for second matrix");
+    for(i=0;i<=2;i++)
+        for(j=0;j<=2;j++)
+        scanf("%d",&B[i][j]);
+    for(i=0;i<=2;i++)
+   
+}
