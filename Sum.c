6c53c3245854da4a1e28e0a3cfbd133e1040afd2
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-29 11:42:05 +0530

    Update Sum.c

--- Sum.c
+++ Sum.c
@@ -11,5 +11,13 @@ main ()
         for(j=0;j<=2;j++)
         scanf("%d",&B[i][j]);
     for(i=0;i<=2;i++)
-   
+    {
+        for(j=0;j<=2;j++)
+    {
+        C[i][j]=A[i][j]+B[i][j];
+        printf("%d \t",C[i][j]);
+    }
+    printf("\n");
+
+    }
 }
