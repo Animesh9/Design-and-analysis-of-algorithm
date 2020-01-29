c981f090c09456edcf45d5f029404001e6ab1851
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-29 11:44:47 +0530

    Update SumOfMatrices.c

--- SumOfMatrices.c
+++ SumOfMatrices.c
@@ -2,14 +2,16 @@
 main ()
 {
     int A[3][3],B[3][3],C[3][3],i,j;
-    printf("Enter 9 numbers for first matrix");
+    //.......Enter 9 Numbers For Both The Matrices......
+    printf("Enter 9 numbers for first matrix ");
     for(i=0;i<=2;i++)
         for(j=0;j<=2;j++)
         scanf("%d",&A[i][j]);
-    printf("Enter 9 numbers for second matrix");
+    printf("Enter 9 numbers for second matrix ");
     for(i=0;i<=2;i++)
         for(j=0;j<=2;j++)
         scanf("%d",&B[i][j]);
+    //......Sum Of Matrices........
     for(i=0;i<=2;i++)
     {
         for(j=0;j<=2;j++)
