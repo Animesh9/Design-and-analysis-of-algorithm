620bc5f2fa552c027a4ce696c3076d69943089de
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-01-30 20:49:05 +0530

    Update IdentifyingVC.c

--- IdentifyingVC.c
+++ IdentifyingVC.c
@@ -3,6 +3,7 @@ int main( )
 {
     char arr[15]="animesh jain";
     int b=0, c=0 ,d=0;
+    //....Identifying Vowels
     while(arr[b]!='\0')
     {
         if(arr[b]=='a'||arr[b]=='e'||arr[b]=='i'||arr[b]=='o'||arr[b]=='u')
