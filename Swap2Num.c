84b0c83bc4d12a86609ce7bbc2690f8aa890ff64
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-01 16:21:40 +0530

    Update Swap2Num.c

--- Swap2Num.c
+++ Swap2Num.c
@@ -6,8 +6,11 @@ int main()
       scanf("%lf", &a);
       printf("Enter second number: ");
       scanf("%lf", &b);
+      //Storing the value of a in temp
       temp = a;
+      //storing value of b in a
       a = b;
+      //Storing the value of temp in b
       b = temp;
       printf("\nAfter swapping, firstNumber = %.2lf\n",a);
       printf("After swapping, secondNumber = %.2lf", b);
