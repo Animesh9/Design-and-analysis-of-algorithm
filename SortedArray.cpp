727783501e4f3359f4f231cd63df96d1bb924ce0
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-03 11:47:25 +0530

    Update SortedArray.cpp

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -9,4 +9,31 @@ void swap(int *x, int *y)
     *x = *y;
     *y = temp;
 }
+int main()
+{
+	int A[10], i, j, n;
+
+	 cout<<"enter the no elements of the array ";
+	 cin>> n;
+
+	 for(i=0;i<n;i++)
+	{
+	  cin>> A[i];
+}
+
+for(i=0;i<n-1;i++)
+{
+for(j=0;j<n-i-1;j++)
+{
+	 if(A[j]<A[j+1])
+	 swap(A[j],A[j+1]);
+
+}
+}
+
+cout<<"the sorted array is ";
+for(i=0;i<n;i++)
+cout<<"\n"<<A[i];
+}
+
 
