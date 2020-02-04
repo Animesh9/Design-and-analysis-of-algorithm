44f8991b8baa681f083d3fd9675c4d6c00b7315c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-04 14:48:25 +0530

    Update GrayCode.c

--- GrayCode.c
+++ GrayCode.c
@@ -1,3 +1,20 @@
 #include<stdio.h>
 #include<math.h>
 int btog(long int);
+int main()
+int btog(long int bin)
+{
+    int a,b,res=0,i=0;
+    while (bin!=0)
+    {
+        a=bin%10;
+        bin=bin/10;
+        b=bin%10;
+        if((a&&!b)||(!a&&b))
+        {
+            res=res+pow(10,i);
+        }
+        i++;
+    }
+    return res;
+}
