aa099550ec7fec1644dd6ed8756218d26e61bd5f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-05 16:35:02 +0530

    Update PotatoesInConveyerBelt.cpp

--- PotatoesInConveyerBelt.cpp
+++ PotatoesInConveyerBelt.cpp
@@ -6,5 +6,22 @@ int main()
      cout << "Enter number of potatoes in each belt ";
         cin >> n;
         int belt[6][n];
+    for(int i=0; i<6; i++)
+    {
+        cout << "Belt-"<<i+1;
+        for(a=0;a<n;a++)
+        {
+            cout<<"\nweight of potatoes";
+            cin>>p;
+            belt[i][a] = p;
+            if(p>=100&&p<=200)
+                c1++;
+            else c2++;
+        }
+        for(int j=0; j<n; j++)
+            cout << belt[i][j]<<endl;
+    }
+    cout<<"right potatoes"<<c1<<endl;
+    cout<<"rotten potatoes"<<c2;
 
 }
