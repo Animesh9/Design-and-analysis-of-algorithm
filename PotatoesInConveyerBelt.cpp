dadfc757d14333cbabdc1f1334fdd8d265d2f479
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-05 16:34:43 +0530

    Update PotatoesInConveyerBelt.cpp

--- PotatoesInConveyerBelt.cpp
+++ PotatoesInConveyerBelt.cpp
@@ -1,2 +1,10 @@
 #include<iostream>
 using namespace std;
+int main()
+{
+    int a,c1=0,c2=0,n,p;
+     cout << "Enter number of potatoes in each belt ";
+        cin >> n;
+        int belt[6][n];
+
+}
