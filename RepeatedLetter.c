f99e6f2127fd1a9df8404afd99db9f8df2839095
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-06 15:33:04 +0530

    Update RepeatedLetter.c

--- RepeatedLetter.c
+++ RepeatedLetter.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+int main()
+{
+    char string[] = "ALWAYS BE COOL";
+    int count;
+    printf("Repeated letter in the given string: \n");
+    printf("%s\n",string);
+
+
+    return 0;
+}
