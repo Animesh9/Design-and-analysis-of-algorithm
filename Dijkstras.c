1c165cbd5e498c6e15657bbe20ba41d8d0e99778
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-07 01:03:35 +0530

    Update Dijkstras.c

--- Dijkstras.c
+++ Dijkstras.c
@@ -5,3 +5,20 @@
 
 int dijkstra(int G[MAX][MAX],int n,int start);
 
+int main()
+{
+	int G[MAX][MAX],i,j,n,u;
+	printf("Enter no. of vertices:");
+	scanf("%d",&n);
+	printf("\nEnter the adjacency matrix:\n");
+
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			scanf("%d",&G[i][j]);
+
+	printf("\nEnter the starting node:");
+	scanf("%d",&u);
+	dijkstra(G,n,u);
+
+	return 0;
+}
