2120e28fed807965927c6f05dd2116ecd7b0e780
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-06 15:33:34 +0530

    Update RepeatedLetter.c

--- RepeatedLetter.c
+++ RepeatedLetter.c
@@ -6,7 +6,22 @@ int main()
     int count;
     printf("Repeated letter in the given string: \n");
     printf("%s\n",string);
-
+    for(int i = 0; i < strlen(string); i++)
+        {
+            count = 1;
+            for(int j = i+1; j < strlen(string); j++)
+            {
+                if(string[i] == string[j] && string[i] != ' ')
+                {
+                    count++;
+                    string[j] = '0';
+                }
+            }
+            if(count>1 && string[i]!='0')
+            {
+                printf("%c\n", string[i]);
+            }
+        }    
 
     return 0;
 }
