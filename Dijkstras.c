1e40d0f5ac220a25bcef86fec310b41ad6589788
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-08 00:42:20 +0530

    Update Dijkstras.c

--- Dijkstras.c
+++ Dijkstras.c
@@ -22,3 +22,56 @@ int main()
 
 	return 0;
 }
+int dijkstra(int G[MAX][MAX],int n,int start)
+{
+	int cost[MAX][MAX],distance[MAX],pred[MAX];
+	int visited[MAX],count,mindistance,nextnode,i,j;
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			if(G[i][j]==0)
+				cost[i][j]=Max1;
+			else
+				cost[i][j]=G[i][j];
+	for(i=0;i<n;i++)
+	{
+		distance[i]=cost[start][i];
+		pred[i]=start;
+		visited[i]=0;
+	}
+
+	distance[start]=0;
+	visited[start]=1;
+	count=1;
+	while(count<n-1)
+	{
+		mindistance=Max1;
+		for(i=0;i<n;i++)
+			if(distance[i]<mindistance&&!visited[i])
+	{
+		mindistance=distance[i];
+		nextnode=i;
+	}
+	visited[nextnode]=1;
+	for(i=0;i<n;i++)
+	if(!visited[i])
+	if(mindistance+cost[nextnode][i]<distance[i])
+	{
+		distance[i]=mindistance+cost[nextnode][i];
+		pred[i]=nextnode;
+	}
+		count++;
+	}
+	for(i=0;i<n;i++)
+		if(i!=start)
+	{
+		printf("\nDistance of node%d=%d",i,distance[i]);
+		printf("\nPath=%d",i);
+
+		j=i;
+		do
+	{
+			j=pred[j];
+			printf("<-%d",j);
+	}while(j!=start);
+	}
+}
