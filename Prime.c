fb03b7db2e42384d01d0922e39f42362555ad6fe
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-16 14:38:50 +0530

    Update Prime.c

--- Prime.c
+++ Prime.c
@@ -3,5 +3,14 @@ int main() {
     int n, i, prime = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
+        for (i = 2; i <= n / 2; ++i) 
+    {
+        if (n % i == 0) 
+        {
+            prime = 1;
+            break;
+        }
+    }
+    
     return 0;
 }
