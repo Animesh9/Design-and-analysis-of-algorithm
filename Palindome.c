31647eb817b99872bbe05efeb72f0e27cdd3ad5d
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-19 15:01:44 +0530

    Update Palindome.c

--- Palindome.c
+++ Palindome.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main() 
+{
+    int n, remainder, ori;
+    int rev = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    ori = n;
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    if (ori== rev)
+        printf("%d is a palindrome.", ori);
+    else
+        printf("%d is not a palindrome.", ori);
+    return 0;
+}
