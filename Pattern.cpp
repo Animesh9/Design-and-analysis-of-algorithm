cef2768527cafed766d701c0041e1ced60e45b96
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-20 15:09:58 +0530

    Update Pattern.cpp

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std:
+int main() 
+{
+    int a,b,rows;
+   cout<<"Enter number of rows: ";
+    cin>>rows;
+    for (a=1; a<=rows; ++a) {
+        for (a=1; b<=a; ++b)
+        { 
+          cout<<b
+        }
+        cout<<\n;
+    }
+    return 0;
+}
