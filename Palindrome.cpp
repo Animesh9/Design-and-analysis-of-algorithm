26a75e7f8d54620e7e96f403bfb4af40bc17b1f1
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-21 10:19:13 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n, remainder, ori;
+    int rev = 0;
+    cout<<"Enter an integer: ;
+    cin>>n;
+    ori = n;
+    while (n != 0) {
+        remainder = n % 10;
+        rev = rev * 10 + remainder;
+        n /= 10;
+    }
+    if (ori== rev)
+        cout<<"%d is a palindrome."<< ori;
+    else
+        cout<<"%d is not a palindrome."<< ori;
+    return 0;
+}
