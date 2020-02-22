b259a3345bed86e8dbba37f8426a0274126c13f2
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-22 20:33:13 +0530

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,1 +1,27 @@
-
+#include <stdio.h>
+int main()
+{
+    int n, i, prime = 0;
+    cout<<"Enter a positive integer: ";
+    cin>>n;
+    for (i = 2; i <= n / 2; ++i) 
+    {
+        if (n % i == 0) 
+        {
+            prime = 1;
+            break;
+        }
+    }
+    if (n == 1) 
+    {
+        cout<<"1 is neither prime nor composite.";
+    }
+    else 
+    {
+        if (prime == 0)
+            cout<<n<< "is a prime number.";
+        else
+            cout<<n<<" is not a prime number.");
+    }
+    return 0;
+}
