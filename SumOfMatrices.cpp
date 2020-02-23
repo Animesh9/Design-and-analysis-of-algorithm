9bb1d9e207a6c87acb6715a7f766b14f96016e83
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-23 13:47:05 +0530

    Update SumOfMatrices.cpp

--- SumOfMatrices.cpp
+++ SumOfMatrices.cpp
@@ -1,1 +1,8 @@
+#include<iostream>
+void main ()
+{
+    int A[3][3],B[3][3],C[3][3],i,j;
+    //.......Enter 9 Numbers For Both The Matrices......
+    cout<<"Enter 9 numbers for first matrix ";
 
+}
