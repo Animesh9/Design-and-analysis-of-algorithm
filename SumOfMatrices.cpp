00d07ba28f79a7f6293bc96608e6c2fadb2a6c97
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-23 13:47:43 +0530

    Update SumOfMatrices.cpp

--- SumOfMatrices.cpp
+++ SumOfMatrices.cpp
@@ -4,5 +4,13 @@ void main ()
     int A[3][3],B[3][3],C[3][3],i,j;
     //.......Enter 9 Numbers For Both The Matrices......
     cout<<"Enter 9 numbers for first matrix ";
+    for(i=0;i<=2;i++)
+        for(j=0;j<=2;j++)
+        cin>>A[i][j];
+    cout<<"Enter 9 numbers for second matrix ";
+    for(i=0;i<=2;i++)
+        for(j=0;j<=2;j++)
+        cin>>B[i][j];
+    //......Sum Of Matrices........
 
 }
