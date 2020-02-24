7b4596e27595d5cf4dcb5ed99be5ef6ff53a7ead
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-24 11:55:15 +0530

    Update Swap2Num.cpp

--- Swap2Num.cpp
+++ Swap2Num.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main() 
 {
-  
+      double a, b, temp;
+      cout<<"Enter first number: ";
+      cin>>a;
+      cout<<"Enter second number: ";
+      cin>>b;
+      temp = a;
+      a = b;
+      b = temp;
+      cout<<"\nAfter swapping, firstNumber = "<<a;
+      cout<<"After swapping, secondNumber = "<< b;
+      return 0;
 }
