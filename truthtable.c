9c71c0a90ccfa3d3da7fde6a9a6322335a0fc79e
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-25 11:38:39 +0530

    Update truthtable.c

--- truthtable.c
+++ truthtable.c
@@ -2,13 +2,13 @@
 int main()
 {
     int x,y,z,s=0;
-    printf("  X  |  Y  |  Z  |  XY+Z  ");
+    printf("  X   |  Y   |   Z   |  XY+Z  ");
     for(x=0;x<=1;++x)
         for(y=0;y<=1;++y)
             for(z=0;z<=1;++z)
             {
                 if(x*y+z==2)
-                    printf("\n  %d   |  %d   |  %d   |   1",x,y,z);
+                    printf("\n  %d   |  %d   |   %d   |   1",x,y,z);
                 else
                 {
                      s=x*y+z;
