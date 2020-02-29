a8a026b362a4193ded726b9e01b92147d6ae4fce
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-02-29 21:25:17 +0530

    Update IdentifyingVC.cpp

--- IdentifyingVC.cpp
+++ IdentifyingVC.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using namespace std;
 int main( )
 {
-  
+  char arr[15]="animesh jain";
+    int b=0, c=0 ,d=0;
+    //....Identifying Vowels and Consonants
+    while(arr[b]!='\0')
+    {
+        if(arr[b]=='a'||arr[b]=='e'||arr[b]=='i'||arr[b]=='o'||arr[b]=='u')
+            c++;
+            else
+            {
+                d++;
+                if (arr[b]== ' ')
+                    d--;
+            }
+
+        b++;
+    }
+    cout<<c;
+    cout<<d;
 }
