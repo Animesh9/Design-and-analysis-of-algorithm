cbb689f05e482786c6c2f7275abc683ff29f8b8f
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-04 00:05:04 +0530

    Update BitwiseOperator.c

--- BitwiseOperator.c
+++ BitwiseOperator.c
@@ -2,3 +2,11 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+void calculate_the_maximum(int n, int k) 
+{
+    int maxAnd = 0;
+    int maxOr = 0;
+    int maxXor = 0;
+    
+    printf("%d\n%d\n%d\n", maxAnd, maxOr, maxXor);
+}
