3efef07053d71a2a5f2f9d50fac2000aeec89aff
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-06 00:40:32 +0530

    main

--- 1DArray.c
+++ 1DArray.c
@@ -2,3 +2,18 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    int *val = malloc(n*sizeof(int));
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&val[i]);
+        sum+=val[i];
+    }
+    printf("%i",sum);
+    free(val);  
+    return 0;
+}
+
