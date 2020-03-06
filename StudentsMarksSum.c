768bc8ec9cfdeaad877916c1ac1ec73f8aa41f9c
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-07 00:13:35 +0530

    main

--- StudentsMarksSum.c
+++ StudentsMarksSum.c
@@ -2,3 +2,25 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+
+
+int main() 
+{
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
