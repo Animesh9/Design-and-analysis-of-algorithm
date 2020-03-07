2fb6c540248f78a725498063ab979361f28e62e7
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-08 00:11:45 +0530

    Main

--- DigitFrequency.c
+++ DigitFrequency.c
@@ -5,7 +5,14 @@
 
 int main() 
 {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    
+    int* nums = (int*) malloc(10 * sizeof(int));
+    char c;    
+    for(int i = 0; i < 10; i++)
+        *(nums+i) = 0;
+    while(scanf("%c", &c) == 1)
+        if(c >= '0' && c <= '9')
+            (*(nums+(c-'0')))++;
+    for(int i = 0; i < 10; i++)
+        printf("%d ", *(nums+i));
+    return 0;
 }
