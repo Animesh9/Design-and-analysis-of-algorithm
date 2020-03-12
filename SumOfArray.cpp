a8899526be46d83abdfa6edc494f27e1b04b4964
Animesh9 <46739641+Animesh9@users.noreply.github.com>
2020-03-13 00:29:32 +0530

    Update SumOfArray.cpp

--- SumOfArray.cpp
+++ SumOfArray.cpp
@@ -5,5 +5,15 @@ int main()
     int n=0;
     cin>>n;
     int** array= new int*[n];
-    
+    for(int i=0;i<n;i++)
+    {
+        array[i]= new int[n];
+        for(int j=0;j<n;j++)
+        {
+            cin>> array[i][j];
+        }
+    }
+      
+    cout<<sum;
+	return 0;
 }
